7c9b59c588e01da3fe1679f79047c6e5e12cbea9
mohammad <mohammadtaheri506@gmail.com>
2019-08-24 13:04:23 +0430

    change style in placeinput and sharePlace

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -4,7 +4,7 @@ import {
   SELECT_PLACE,
   DESELECT_PLACE
 } from "../actions/actionTypes";
-
+import Image from '../../assets/beautiful-place.jpg'
 const initialState = {
   places: [],
   selectedPlace: null
@@ -18,10 +18,7 @@ const reducer = (state = initialState, action) => {
         places: state.places.concat({
           key: Math.random(),
           name: action.placeName,
-          image: {
-            uri:
-              "https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg"
-          }
+          image: Image
         })
       };
     case DELETE_PLACE:
